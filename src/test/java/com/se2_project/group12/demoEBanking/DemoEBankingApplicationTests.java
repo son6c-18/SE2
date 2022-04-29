8e7240a6b5991254cfc26b573ebf836aa85dfc30
tuasnson <102129029+son6c-18@users.noreply.github.com>
2022-04-29 19:51:48 +0700

    chace

---
+++ src/test/java/com/se2_project/group12/demoEBanking/DemoEBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2_project.group12.demoEBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
